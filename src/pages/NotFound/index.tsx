4e4a5d41c7d8b52f89071a013ae3eb7bf63fc2ac
Alex <alex.horn.98@mail.ru>
2021-08-05 11:46:20 +0600

    add pages structure

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+    <div>
+      <p>not</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return (
+    <div>
+      <p>sign up</p>
+    </div>
+  );
+};
+
+export default SignUp;
